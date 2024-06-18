2986da8c93d96bb0fae6dc13e6e662dade68fe4e
Rodrigo Rojas <rodrigo.rjsmz@gmail.com>
2024-06-18 17:50:23 -0400

    part 3.6

--- index.js
+++ index.js
@@ -41,6 +41,31 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing',
+    })
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique',
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 100000),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter((person) => person.id !== id)
