8f995025005b3dce145adb1c788224d72ed504fd
Rodrigo Rojas <rodrigo.rjsmz@gmail.com>
2024-06-24 16:40:41 -0400

    part 3.14

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+
+const Person = require('./models/person')
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
@@ -11,42 +14,16 @@ app.use(
 )
 app.use(express.static('dist'))
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then((persons) => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find((person) => person.id === id)
-
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -58,20 +35,14 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  if (persons.find((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: 'name must be unique',
-    })
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 100000),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-  res.json(person)
+  person.save().then((savedPerson) => {
+    res.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -88,7 +59,7 @@ app.get('/info', (req, res) => {
   )
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
