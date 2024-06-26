1f229cf1150136c4daa02d17178c638bb931d25b
Rodrigo Rojas <rodrigo.rjsmz@gmail.com>
2024-06-26 17:13:37 -0400

    part 3.22

--- backend/index.js
+++ backend/index.js
@@ -61,9 +61,9 @@ app.put('/api/persons/:id', (req, res, next) => {
     .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-    .then((result) => {
+    .then(() => {
       res.status(204).end()
     })
     .catch((error) => next(error))
